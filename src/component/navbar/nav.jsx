f2e5da5c46d3b890912e88093959646b8403d34d
Vishal Jangid <vishal.jangid_it22@pccoer.in>
2024-01-23 10:58:02 +0530

    Nav bar changes

--- src/component/navbar/nav.jsx
+++ src/component/navbar/nav.jsx
@@ -1,18 +1,28 @@
 import React from 'react'
 import "./nav.css";
-import Btn1 from "./buttons/Button1"
+import logo from "../../images/logo.png";
+import {Home} from 'lucide-react';
+import { PanelsTopLeft } from 'lucide-react';
+import {Phone} from 'lucide-react';
+import { GanttChartSquare } from 'lucide-react';
+
 
 const nav = () => {
   return (
+    <div className="nav-wrapper">
     <div className='nav'>
-        <img src="" alt="" srcset="" />
+        <img src={logo} alt="" srcset="" />
         <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Profile</li>
-            <li>Services</li>
-            <li>Contact</li>
+            <li><Home />
+            <p>Home</p></li>
+            {/* <li>About</li> */}
+            <li><PanelsTopLeft/>
+            <p>Profile</p></li>
+            <li><GanttChartSquare />
+            <p>Services</p></li>
+            <li><Phone /><p>Contact us</p></li>
         </ul>
+    </div>
     </div>
   )
 }
